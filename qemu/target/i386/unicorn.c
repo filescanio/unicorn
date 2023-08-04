209f06b02af73b7fdf96f5960e921bcb48d49dab
zraq <richard.quotes@opswat.com>
2023-08-04 22:22:16 +1000

    rdtsc/cpuid hooks
    cherry pick of 1.x commit b3014eb72c4dae69bc3bb85e05c2a529cc96df94.

--- qemu/target/i386/unicorn.c
+++ qemu/target/i386/unicorn.c
@@ -2003,7 +2003,7 @@ static bool x86_insn_hook_validate(uint32_t insn_enum)
     // for x86 we can only hook IN, OUT, and SYSCALL
     if (insn_enum != UC_X86_INS_IN && insn_enum != UC_X86_INS_OUT &&
         insn_enum != UC_X86_INS_SYSCALL && insn_enum != UC_X86_INS_SYSENTER &&
-        insn_enum != UC_X86_INS_CPUID) {
+        insn_enum != UC_X86_INS_CPUID && insn_enum != UC_X86_INS_RDTSC) {
         return false;
     }
     return true;
