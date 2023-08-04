a6803f15e2e1d2cc90bc9f451d29d06c9564fcff
zraq <richard.quotes@opswat.com>
2023-08-04 22:22:16 +1000

    rdtsc/cpuid hooks
    cherry pick of 1.x commit b3014eb72c4dae69bc3bb85e05c2a529cc96df94.

--- include/unicorn/x86.h
+++ include/unicorn/x86.h
@@ -77,13 +77,25 @@ typedef struct uc_x86_msr {
 // @user_data: user data passed to tracing APIs.
 typedef void (*uc_cb_insn_syscall_t)(struct uc_struct *uc, void *user_data);
 
-// Callback function for tracing cpuid (for uc_hook_intr())
-// @user_data: user data passed to tracing APIs.
-//
-// @return: true indicates the callback overwrites the cpuid instruction while
-// false
-//          indicates cpuid instruction will still be executed.
-typedef int (*uc_cb_insn_cpuid_t)(struct uc_struct *uc, void *user_data);
+typedef struct uc_x86_cpuid_q
+{
+   uint32_t in_eax;
+   uint32_t in_ecx;
+   uint32_t out_eax;
+   uint32_t out_ebx;
+   uint32_t out_ecx;
+   uint32_t out_edx;
+} uc_x86_cpuid_q;
+
+typedef void (*uc_cb_insn_cpuid_t)(struct uc_struct *uc, struct uc_x86_cpuid_q* cpuid_query, void *user_data);
+
+typedef struct uc_x86_rdtsc_q
+{
+   uint32_t eax;
+   uint32_t edx;
+} uc_x86_rdtsc_q;
+
+typedef void (*uc_cb_insn_rdtsc_t)(struct uc_struct *uc, struct uc_x86_rdtsc_q* rdtsc_query, void *user_data);
 
 //> X86 registers
 typedef enum uc_x86_reg {
